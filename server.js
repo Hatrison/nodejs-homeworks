9d6c82363279c9c4aec22cb510b11be7950692b2
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-07-19 15:35:04 +0300

    Added connection to database

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server is running on address http://localhost:3000");
-});
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server is running on address http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
